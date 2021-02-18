6ae7196ec9de7882b818c96b23282744385022d3
NathanKarafotia <nathan.karafotias@ocsbstudent.ca>
2021-02-18 21:41:33 +0000

    cammont

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// tells function to work
 document.getElementById('button').addEventListener('click',compare)
 let userinput = 0
 
+// gets the number from box and will check if its positive or negative
 function compare() {
   userinput = document.getElementById('input').value
   userinput = parseInt(userinput)
