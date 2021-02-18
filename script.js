8999dda3c031de9106c7630fb7c01d82ea051333
NathanKarafotia <nathan.karafotias@ocsbstudent.ca>
2021-02-18 21:37:50 +0000

    stuff

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ document.getElementById('button').addEventListener('click',compare)
 let userinput = 0
 
 function compare() {
-  userinput = document.getElementById('input').Value
+  userinput = document.getElementById('input').value
   userinput = parseInt(userinput)
 
 
   if (userinput < 0) {
     document.getElementById('answer').innerHTML = "negative"
-  } else {
+   } else {
     document.getElementById('answer').innerHTML = "positive"
-  }
+   }
 
 }
